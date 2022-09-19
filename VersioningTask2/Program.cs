80423674f44b531693df1a03922e8dbedc911d4c
fasdser <107477645+fasdser@users.noreply.github.com>
2022-09-19 21:12:47 +0300

    Add project files.

---
+++ VersioningTask2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersioningTask2
+{
+
+    abstract class BaseClass
+    {
+        protected abstract void Read();
+        protected abstract void Write();
+
+        public void ReadWrite()
+        {
+            Read();
+            Write();
+        }
+    }
+
+    class DerivedClassTXT : BaseClass
+    {
+        protected override void Read()
+        {
+            Console.WriteLine("Чтение из файла TXT");
+        }
+        protected override void Write()
+        {
+            Console.WriteLine("Запись в файл TXT");
+        }
+    }
+
+    class DerivedClassXML : BaseClass
+    {
+        protected override void Read()
+        {
+            Console.WriteLine("Чтение из файла XML");
+        }
+
+        protected override void Write()
+        {
+            Console.WriteLine("Запись в файл XML");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BaseClass> list = new List<BaseClass>() { new DerivedClassTXT(),new DerivedClassXML()};
+            foreach (BaseClass c in list)
+                c.ReadWrite();
+            Console.ReadLine();
+        }
+    }
+}
